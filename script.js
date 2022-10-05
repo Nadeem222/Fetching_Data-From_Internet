3148337682e92fd056702f283ab4c6cc2a565101
DESKTOP-1QJ3QEI <syednadeemali221@gmail.com>
2022-10-05 15:46:54 +0500

    make app

---
+++ script.js
@@ -0,0 +1,4 @@
+let timeEl =document.getElementById('time');
+let dateEl = document.getElementById('date');
+let cureentWeatherItemEl = document.getElementById('current-weather-item');
+let timeZOne = document.getElementById('time-zone');
